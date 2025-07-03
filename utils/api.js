b7d81b8355ac52b9d1177a3ffc46f57ee7eb7871
Vasudha Kumari <vasudhagupta2003@gmail.com>
2025-07-04 00:43:49 +0530

    Added frontend deployed link

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5175/api", // change if deployed
+  baseURL: "https://signee.netlify.app/api", // change if deployed
 });
 
 export default API;
