ec8c7becca7e9819df26373ffc6c22927b21b1f2
Vasudha Kumari <vasudhagupta2003@gmail.com>
2025-07-04 00:22:51 +0530

    again fixed

--- index.js
+++ index.js
@@ -40,5 +40,5 @@ mongoose
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running onn http://localhost:${PORT}`);
 });
